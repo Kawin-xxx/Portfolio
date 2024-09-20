1f37aca38760dd03720426bd18127f03ffdfcace
Ekaterina <ekaterina589273@yandex.ru>
2024-09-20 08:56:43 +0700

    Add toggle menu

--- portfolio/src/components/MainHeader.vue
+++ portfolio/src/components/MainHeader.vue
@@ -1,6 +1,16 @@
 <script>
 
 export default {
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        }
+    },
 }
 </script>
 
@@ -14,29 +24,40 @@ export default {
             </div>
 
             <nav class="header__navigation navigation">
-                <button class="navigation__toggle navigation__toggle--close" type="button">
-                    <span class="visually-hidden">Открыть меню</span>
+                <button
+                    class="navigation__toggle"
+                    :class="{ 'navigation__toggle--open': isMenuOpen, 'navigation__toggle--close': !isMenuOpen }"
+                    type="button"
+                    @click="toggleMenu"
+                    :aria-expanded="isMenuOpen"
+                >
+                    <span class="visually-hidden">
+                        {{ isMenuOpen ? 'Закрыть меню' : 'Открыть меню' }}
+                    </span>
                 </button>
-                <ul class="navigation__list site-list">
-                    <li class="site-list__item">
-                        <a href="" class="site-list__link">Home</a>
-                    </li>
-                    <li class="site-list__item">
-                        <a href="" class="site-list__link">Education</a>
-                    </li>
-                    <li class="site-list__item">
-                        <a href="" class="site-list__link">Services</a>
-                    </li>
-                    <li class="site-list__item">
-                        <a href="" class="site-list__link">Contact</a>
-                    </li>
-                </ul>
+
+                <transition name="fade">
+                    <ul class="navigation__list site-list" v-if="isMenuOpen">
+                        <li class="site-list__item">
+                            <a href="" class="site-list__link">Home</a>
+                        </li>
+                        <li class="site-list__item">
+                            <a href="" class="site-list__link">Education</a>
+                        </li>
+                        <li class="site-list__item">
+                            <a href="" class="site-list__link">Services</a>
+                        </li>
+                        <li class="site-list__item">
+                            <a href="" class="site-list__link">Contact</a>
+                        </li>
+                    </ul>
+                </transition>
             </nav>
         </div>
     </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .header__container {
     display: flex;
     justify-content: space-between;
@@ -46,13 +67,75 @@ export default {
     list-style: none;
     display: flex;
     column-gap: 30px;
+    background-color: white;
+}
+
+.navigation__toggle {
+  border: none;
+  position: relative;
+  background-color: inherit;
+  width: 60px;
+  height: 60px;
+  cursor: pointer;
 }
 
 .navigation__toggle--close {
-    display: none;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 24px;
+    right: 22px;
+    height: 1.5px;
+    width: 16px;
+    background-color: white;
+    box-shadow: 0 5px 0 0 white, 0 10px 0 0 white;
+    // background-color: $color-black;
+    // box-shadow: 0 5px 0 0 $color-black, 0 10px 0 0 $color-black;
+  }
 }
 
 .navigation__toggle--open {
+  &::before {
+    content: "";
+    position: absolute;
+    top: 30px;
+    right: 22px;
+    height: 1.5px;
+    width: 16px;
+    transform: rotate(45deg);
+    // background-color: $color-black;
+    background-color: white;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 30px;
+    right: 22px;
+    height: 1.5px;
+    width: 16px;
+    transform: rotate(-45deg);
+    // background-color: $color-black;
+    background-color: white;
+  }
+}
+
+.navigation__list {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    z-index: 2;
+    top: 60px;
     display: block;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
