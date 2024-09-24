22e27fddfece26bb61ff0d326577f78e82c5a01e
Ekaterina <ekaterina589273@yandex.ru>
2024-09-25 04:12:35 +0700

    Change grid for experience section

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-    <section class="experience section" id="experience">
+    <section class="main-container__experience section experience" id="experience">
         <div class="experience__container container">
             <h2 class="experience__title">Мой опыт</h2>
             <ul class="experience__list">
@@ -30,4 +30,30 @@ export default {
 </template>
 
 <style scoped lang="scss">
+.experience__container {
+    margin: 0 auto;
+    padding: 50px 20px 30px;
+}
+
+.experience__title {
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+.experience__list {
+    list-style: none;
+    padding-left: 20px;
+    padding-top: 5px;
+    position: relative;
+
+    &::before {
+        width: 2px;
+        height: 100%;
+        background-color: $color-white;
+        position: absolute;
+        left: 0;
+        top: 0;
+        content: "";
+    }
+}
 </style>
